d2ab9f72dd44921fbaa956a02f8c432fbb7a333b
Osama <osamaaljrmi@gmail.com>
2026-03-15 04:25:27 +0300

    Fix Playwright for Render headless mode

--- app/musaned/browser.py
+++ app/musaned/browser.py
@@ -2,14 +2,17 @@ from playwright.sync_api import sync_playwright, Browser, BrowserContext, Page
 from typing import Tuple
 
 
-def launch_browser(headless: bool = False) -> Tuple[Browser, BrowserContext, Page]:
+def launch_browser(headless: bool = True) -> Tuple[Browser, BrowserContext, Page]:
     playwright = sync_playwright().start()
 
     browser = playwright.chromium.launch(
-        headless=headless,
+        headless=True,
         args=[
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu",
             "--disable-blink-features=AutomationControlled",
-            "--start-maximized",
         ],
     )
 
